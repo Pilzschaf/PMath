93cc6971fcf6490fc175298bae324769d5c9d808
Lenpai <42583215+lenpai@users.noreply.github.com>
2018-11-21 14:48:53 +0100

    small performance imporovements in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,27 @@
 using namespace pmath;
 
 void print(Vector2 v) {
-    std::cout << "(" << v.x << "," << v.y << ")" << std::endl;
+    std::cout << '(' << v.x << ',' << v.y << ")\n";
 }
 
 void print(Vector3 v) {
-    std::cout << "(" << v.x << "," << v.y << "," << v.z << ")" << std::endl;
+    std::cout << '(' << v.x << ',' << v.y << ',' << v.z << ")\n";
 }
 
 void print(Vector4 v) {
-    std::cout << "(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")" << std::endl;
+    std::cout << '(' << v.x << ',' << v.y << ',' << v.z << ',' << v.w << ")\n";
 }
 
 int main(int argc, char** argv) {
     pmath::Vector2 position(1.0f, 2.0f);
     Vector4 direction(1.0f, 2.0f, 3.0f, 4.0f);
     Vector3 p(position, 27.0f);
-    std::cout << direction.x << "," << direction.y << std::endl;
+    std::cout << direction.x << ',' << direction.y << '\n';
     for(int i = 0; i < 4; i++) {
         direction[i] = i*2;
     }
     for(int i = 0; i < 4; i++) {
-        std::cout << direction[i] << std::endl;
+        std::cout << direction[i] << '\n';
     }
     std::cout << "Direction: ";
     print(direction);
